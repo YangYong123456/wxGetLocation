ca93464c5e775004ee1ba41c3117a001a5fc3717
YangYong123456 <2215841325@qq.com>
2019-12-03 12:45:09 +0800

    1

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,47 @@
 <template>
   <div>
+    <i-grid i-class="no-border">
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/images/1.png" />
+        </i-grid-icon>
+        <i-grid-label>饮品</i-grid-label>
+    </i-grid-item>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/images/2.png" />
+        </i-grid-icon>
+        <i-grid-label>主食</i-grid-label>
+    </i-grid-item>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/images/3.png" />
+        </i-grid-icon>
+        <i-grid-label>小吃</i-grid-label>
+    </i-grid-item>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/images/4.png" />
+        </i-grid-icon>
+        <i-grid-label>外卖</i-grid-label>
+    </i-grid-item>
+    </i-grid>
+    <i-panel title="标题">
+      <view style="padding: 15px;">
+        <i-card i-class="split" title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+            <view slot="content">内容不错</view>
+            <view slot="footer">尾部内容</view>
+        </i-card>
+        <i-card i-class="split" title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+            <view slot="content">内容不错</view>
+            <view slot="footer">尾部内容</view>
+        </i-card>
+        <i-card i-class="split" title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+            <view slot="content">内容不错</view>
+            <view slot="footer">尾部内容</view>
+        </i-card>
+      </view>
+    </i-panel>
   </div>
 </template>
 
@@ -23,4 +65,10 @@ export default {
 </script>
 
 <style scoped>
+div >>> .no-border{
+  border-width: 0pt;
+}
+div >>> .split{
+  margin-bottom: 10pt;
+}
 </style>
