0c9d6d84fad1e9af85d04f4a27a375786e7553ab
YangYong123456 <2215841325@qq.com>
2019-12-09 15:39:51 +0800

    3

--- src/pages/list/index.vue
+++ src/pages/list/index.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+      <i-card v-for="item in list" :key="item" i-class="split" :extra="item.name" :thumb="item.image">
+          <view slot="content">{{item.remark}}</view>
+          <view slot="footer">{{item.address}}</view>
+      </i-card>
   </div>
 </template>
 
@@ -27,4 +31,7 @@ export default {
 </script>
 
 <style scoped>
+div >>> .split {
+  margin-bottom: 10pt;
+}
 </style>
